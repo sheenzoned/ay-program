66f792a2f9b7976a61fcbeac9869ba446504f603
Sheen Carbellido <38741890+sheenzoned@users.noreply.github.com>
2022-04-04 21:03:28 +0800

    Initial

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,16 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Cebu City SDA Youth</title>
+        <link rel="icon" href="/ay.png" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Join us in our AY Fellowship!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          April 9, 2022 <br/>
+          3:00 PM - 6:00 PM <br/>
+          Cebu City SDA Church
         </p>
       </main>
 
